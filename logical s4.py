9779e5b754f4c23be0e4ba5cd8a6127030b3bb25
srrajesh113 <35676511+srrajesh113@users.noreply.github.com>
2018-01-31 10:32:40 +0530

    Add files via upload

---
+++ logical s4.py
@@ -0,0 +1,32 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> (2+3)*4+8-6**2
+-8
+>>> 2==2
+True
+>>> 
+=============== RESTART: /home/cs2017a207/rajesh/nested s4.py ===============
+3
+>>> 
+=============== RESTART: /home/cs2017a207/rajesh/nested s4.py ===============
+3
+>>> 
+=============== RESTART: /home/cs2017a207/rajesh/nested s4.py ===============
+3
+>>> 
+=============== RESTART: /home/cs2017a207/rajesh/nested s4.py ===============
+>>> 
+=============== RESTART: /home/cs2017a207/rajesh/nested s4.py ===============
+3
+>>> binary(3)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    binary(3)
+NameError: name 'binary' is not defined
+>>> 2 and 3
+3
+>>> 
+>>> 2 or 3
+2
+>>> 
---
+++ nested s4.py
@@ -0,0 +1,8 @@
+a=7
+if(a<15):
+    print('3')
+    if(a<5):
+        print("5")
+        if(a==7):
+            print("7")
+
