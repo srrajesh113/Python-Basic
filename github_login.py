1b24841c6faf7365287a6eccb11088ae252be4d0
RAJESH KUMAR N <35676511+srrajesh113@users.noreply.github.com>
2018-03-25 18:02:46 +0530

    Update github_login.py

--- github_login.py
+++ github_login.py
@@ -7,6 +7,6 @@ user=browser.find_element_by_xpath('//*[@id="login_field"]')
 user.send_keys("rajesh.17cs@cmr.edu.in")
 #typing the password
 password=browser.find_element_by_xpath('//*[@id="password"]')
-password.send_keys("rajesh123$")
+password.send_keys("")
 #login in the fb account
 lb=browser.find_element_by_xpath('//*[@id="login"]/form/div[3]/input[3]').click()
